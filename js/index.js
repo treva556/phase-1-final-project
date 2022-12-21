cfe9eb34d95340e0fdda31015ce41dfa473948dd
treva556 <marktreva92@gmail.com>
2022-12-21 06:12:03 +0300

    continued

--- js/index.js
+++ js/index.js
@@ -1,107 +1,69 @@
+let navbar = document.querySelector('.header .navbar');
 
-//for spinner buttons  
+document.querySelector('#menu-btn').onclick = () =>{
+   navbar.classList.toggle('active');
+}
 
-$(function ($) {
-    $('#handleCounter1').handleCounter({
- minimum: 1,
- maximize: 100,
+window.onscroll = () =>{
+   navbar.classList.remove('active');
+}
 
-        
- })
-       
-     $('#handleCounter2').handleCounter({
- minimum: 1,
- maximize: 100,
-          
-        })
-    
-  
-    
-    })
-//for datepicker
-
-addEventListener('DOMContentLoaded', function () {
- pickmeup('.range', {
-     mode : 'range',
-     separator: '    to   ',
-     position :'bottom',
-//        hide_on_select : true,
-     format: 'd b-y',
-   
- });
-//pickmeup('.range').show();
-
-
- 
+document.querySelectorAll('.contact .row .faq .box h3').forEach(faqBox => {
+   faqBox.onclick = () =>{
+      faqBox.parentElement.classList.toggle('active');
+   }
 });
 
+var swiper = new Swiper(".home-slider", {
+   loop:true,
+   effect: "coverflow",
+   spaceBetween: 30,
+   grabCursor: true,
+   coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: false,
+   },
+   navigation: {
+     nextEl: ".swiper-button-next",
+     prevEl: ".swiper-button-prev",
+   },
+});
 
-// map
-function initMap() {
-     var uluru = {lat:  19.735121, lng: -155.014044};  
-     var map = new google.maps.Map(document.getElementById('map'), {
-       zoom: 14,
-       center: uluru
-     });
-     var marker = new google.maps.Marker({
-       position: uluru,
-       map: map
-     });
-   }
-$(document).ready(function(){
- 
+var swiper = new Swiper(".gallery-slider", {
+   loop:true,
+   effect: "coverflow",
+   slidesPerView: "auto",
+   centeredSlides: true,
+   grabCursor: true,
+   coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2,
+      slideShadows: true,
+   },
+   pagination: {
+      el: ".swiper-pagination",
+    },
+});
 
-             $('.single').slick({
-             autoplay : true,
-               pauseOnFocus : false,
-                 pauseOnHover : false,
-              arrows : false,
-                 draggable : false,
-                      });
-           
-             $('.left').click(function(){
-               $('.single').slick('slickPrev');
-             })
-
-             $('.right').click(function(){
-               $('.single').slick('slickNext');
-             })
-             
-             
-             
-              
-             $('.room').slick({
-             autoplay : false,
-               slidesToShow :3,
-               pauseOnFocus : true,
-             
-              arrows : false,
-                 draggable : true,
-              swipeToSlide : true,
-               });
- 
-             
-             $('.review').slick({
-                 
-             autoplay : true,
-                 autoplaySpeed : 6000,
-               pauseOnFocus : true,
-              pauseOnHover : false,
-              arrows : false,
-                 draggable : true,
-          
-                 
-                      })
-     })
-$(function () {
-$.scrollUp({
- scrollName: 'scrollUp', // Element ID
- topDistance: '600' , // Distance from top before showing element (px)
- topSpeed: 300 , // Speed back to top (ms)
- animation: 'fade', // Fade, slide, none
- animationInSpeed: 500, // Animation in speed (ms)
- animationOutSpeed: 500, // Animation out speed (ms)
-scrollText: '^',
- activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
-})
-})
+var swiper = new Swiper(".reviews-slider", {
+   loop:true,
+   slidesPerView: "auto",
+   grabCursor: true,
+   spaceBetween: 30,
+   pagination: {
+      el: ".swiper-pagination",
+   },
+   breakpoints: {
+      768: {
+        slidesPerView: 1,
+      },
+      991: {
+        slidesPerView: 2,
+      },
+   },
+});
