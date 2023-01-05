3e0961373f6d417e988398ae31df540a513bd8ec
treva556 <marktreva92@gmail.com>
2023-01-05 19:49:52 +0300

    edited

--- js/index.js
+++ js/index.js
@@ -1,7 +1,62 @@
-let navbar = document.querySelector('.Book');
 
-document.querySelector('#menu-btn').onclick = () =>{
-   navbar.classList.toggle('active');
+fetch('http://localhost:3000/rooms', {
+  method: 'PATCH',
+  body: JSON.stringify({
+    title: 'foo',
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
+
+
+
+
+
+function bookRoom() {
+let button = document.querySelector("#book")
+button.addEventListener("click", () => {
+
+
+if(number >=1){
+ currentListing.textContent = currentListing.textContent -1}
+ else {document.querySelector("#book").textContent = "No More Reservations"
+   alert("No More Reservations")}
+}
+)
 }
 
+bookRoom();
 
+function cancelRoom() {
+   let button = document.querySelector("#cancel")
+   button.addEventListener("click", () => {
+   
+   
+   if(number >=1){
+    currentListing.textContent = currentListing.textContent +1}
+     {document.querySelector("#cancel").textContent = "No More Reservations"
+      alert("Booking Canceled")}
+   }
+   )
+   }
+   
+   cancelRoom();
+
+   function checkoutRoom() {
+      let button = document.querySelector("#Check-out")
+      button.addEventListener("click", () => {
+      
+      
+      if(number >=1){
+       currentListing.textContent = currentListing.textContent +1}
+       }
+      
+      )
+   }
+      
+      checkoutRoom();
+   
